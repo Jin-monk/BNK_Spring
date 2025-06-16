44e8d03b8c0c8ec32482d941fa8e7f5e0d08c186
Jin-monk <beobjin-_-@naver.com>
2025-06-16 17:48:27 +0900

    JWT 로그인 인증,인가

---
+++ jwt02/src/test/java/com/example/jwt02/Jwt02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwt02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jwt02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
